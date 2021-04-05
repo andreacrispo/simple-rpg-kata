910f304ce0d3d97c2e2be30dcb00c40a48f4de89
Andrea <andrea.crispo.cr@gmail.com>
2021-04-05 21:33:14 +0200

    changed boolean column value

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -6,8 +6,8 @@ INSERT INTO Users (username) VALUES
     ('Axel');
 
 INSERT INTO Characters (level_value, is_connected, positionx, positiony, hp, class_id, user_id) VALUES
-  (1, 0, 500, 500, 150, 1, 1),
-  (2, 0, 1000, 800, 100, 3, 2),
-  (3, 0, 1200, 500, 120, 2, 3);
+  (1, false, 500, 500, 150, 1, 1),
+  (2, false, 1000, 800, 100, 3, 2),
+  (3, false, 1200, 500, 120, 2, 3);
 
 
